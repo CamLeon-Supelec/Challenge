ee21f73b3a89e8e6dfb51e8f6f23319fc91b1827
delmassad <matthieu.Delmas@polynom.io>
2018-11-09 23:49:32 +0100

    Def d'un file loader def de parse_processes, qui retourne les couples de pro pour un fichier def de parse_sequences, qui retourne les triples de seq pour un fichier

---
+++ utility/file_loader.py
@@ -0,0 +1,26 @@
+from constants import *
+from os import listdir
+from os.path import isfile, join
+
+def file_loader():
+    list_sequence_files = [f for f in listdir(VALIDATION_DIR) if
+                           (isfile(join(VALIDATION_DIR, f)) and f[-23:] == 'behavior_sequence.txt')]
+    list_process_files = [f for f in listdir(VALIDATION_DIR) if
+                          (isfile(join(VALIDATION_DIR, f)) and f[-14:] == 'generation.txt')]
+    return (list_sequence_files, list_process_files)
+
+
+def parse_processes(process_file):
+        with open(process_file) as infile:
+            processes_couples = []
+            for line in infile:
+                processes_couples += [(line.split(' -> ')[0], line.split(' -> ')[1])]
+        return processes_couples
+
+
+def parse_sequences(sequence_file):
+    with open(sequence_file) as infile:
+        sequences_triplets = []
+        for line in infile:
+            sequences_triplets += [line]
+    return sequences_triplets
