e6ffc6acf854fd609046d20b04cc5b74148aedfb
delmassad <matthieu.Delmas@polynom.io>
2018-11-10 00:32:25 +0100

    Ajout d'un label loader

--- utility/file_loader.py
+++ utility/file_loader.py
@@ -6,12 +6,27 @@ from utility.constants import *
 from os import listdir
 from os.path import isfile, join
 
+
 def file_loader():
-    list_sequence_files = [f for f in listdir(VALIDATION_DIR) if
-                           (isfile(join(VALIDATION_DIR, f)) and f[-23:] == 'behavior_sequence.txt')]
-    list_process_files = [f for f in listdir(VALIDATION_DIR) if
-                          (isfile(join(VALIDATION_DIR, f)) and f[-14:] == 'generation.txt')]
-    return list_sequence_files, list_process_files
+    """""
+    Returns 3 lists :
+        list_sequence_files is the list of sequence_filenames
+        list_process_files is the list of processes_filenames
+        file_labels is a list of bools where file_labels[i] == 1 if the ith file is a malware
+    """""
+    list_sequence_files = [f for f in listdir(TRAINING_DIR) if
+                           (isfile(join(TRAINING_DIR, f)) and f[-23:] == 'behavior_sequence.txt')]
+    list_process_files = [f for f in listdir(TRAINING_DIR) if
+                          (isfile(join(TRAINING_DIR, f)) and f[-14:] == 'generation.txt')]
+    file_labels = parse_label(f"{MAIN_DIR}/{LABEL_FILE_NAME}")
+    return list_sequence_files, list_process_files, file_labels
+
+def parse_label(label_file) :
+    with open(label_file) as infile:
+        labels = []
+        for line in infile :
+            labels += [line]
+        return labels
 
 
 def parse_processes(process_file):
@@ -19,7 +34,7 @@ def parse_processes(process_file):
             processes_couples = []
             for line in infile:
                 processes_couples += [(line.split(' -> ')[0], line.split(' -> ')[1])]
-        return processes_couples
+            return processes_couples
 
 
 def parse_sequences(sequence_file):
@@ -27,4 +42,4 @@ def parse_sequences(sequence_file):
         sequences_triplets = []
         for line in infile:
             sequences_triplets += [line]
-    return sequences_triplets
+        return sequences_triplets
