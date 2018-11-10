bac6cc88d32623247110c5b59e9444112c1903eb
delmassad <matthieu.Delmas@polynom.io>
2018-11-10 10:47:13 +0100

    meilleur finale

--- final.py
+++ final.py
@@ -4,9 +4,9 @@ from utility.constants import *
 from utility.file_loader import *
 
 
-def final_function(num_fichier, data) :
-    sequences = parse_sequences(data[1][num_fichier])
-    processes = parse_processes(data[0][num_fichier])
+def final_function(data_i) :
+    sequences = parse_sequences(data_i[1])
+    processes = parse_processes(data_i[0])
     tree, source = generate_subprocess_tre(processes)
     forest = generate_API_behavior_graph(sequences)
     return forest_input(tree, source, forest)
