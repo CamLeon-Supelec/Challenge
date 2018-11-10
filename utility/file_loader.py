d2642ce5564e65c40e37642907148802bb6ab7d1
delmassad <matthieu.Delmas@polynom.io>
2018-11-10 03:09:44 +0100

    addition d'un param n_first, permet de charcger les n_premiers elements

--- utility/file_loader.py
+++ utility/file_loader.py
@@ -7,28 +7,35 @@ from os import listdir
 from os.path import isfile, join
 
 
-def file_loader():
+def file_loader(n_first=0):
     """""
     Returns a list of triples :
         triple[0] = sequence_file_name for the ith software
         triple[1] = process_file_name for the ith software
         triple[2] = label of the ith file 
     """""
-    list_sequence_files = [f for f in listdir(TRAINING_DIR) if
-                           (isfile(join(TRAINING_DIR, f)) and f[-23:] == 'behavior_sequence.txt')]
-    list_process_files = [f for f in listdir(TRAINING_DIR) if
-                          (isfile(join(TRAINING_DIR, f)) and f[-14:] == 'generation.txt')]
-    file_labels = parse_label(f"{MAIN_DIR}/{LABEL_FILE_NAME}")
+    if int == 0:
+        list_sequence_files = [f for f in listdir(TRAINING_DIR) if
+                               (isfile(join(TRAINING_DIR, f)) and f[-23:] == 'behavior_sequence.txt')]
+        list_process_files = [f for f in listdir(TRAINING_DIR) if
+                              (isfile(join(TRAINING_DIR, f)) and f[-14:] == 'generation.txt')]
+        file_labels = parse_label(f"{MAIN_DIR}/{LABEL_FILE_NAME}")
+    else :
+        list_sequence_files = [f for f in listdir(TRAINING_DIR)[:n_first] if
+                               (isfile(join(TRAINING_DIR, f)) and f[-23:] == 'behavior_sequence.txt')]
+        list_process_files = [f for f in listdir(TRAINING_DIR)[:n_first] if
+                              (isfile(join(TRAINING_DIR, f)) and f[-14:] == 'generation.txt')]
+        file_labels = parse_label(f"{MAIN_DIR}/{LABEL_FILE_NAME}")
     results = [(0, 0, 0)] * len(list_sequence_files)
     for i in range(len(list_sequence_files)):
         results[i] = [(list_sequence_files[i], list_process_files[i], file_labels[i])]
     return results
 
-def parse_label(label_file):
+def parse_label(label_file, n_first):
     with open(label_file) as infile:
         labels = []
         for line in infile:
-            labels = list(line)
+            labels = list(line)[:n_first]
         return labels
 
 
