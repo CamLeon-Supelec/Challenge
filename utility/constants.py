893502ec1722932fc264ab412bcffa8d06095e44
delmassad <matthieu.Delmas@polynom.io>
2018-11-10 14:45:02 +0100

    lkjfqsldfjhqsdlkjfhqdf

--- utility/constants.py
+++ utility/constants.py
@@ -3,6 +3,6 @@
 ###################################
 
 MAIN_DIR = f"/home/cloud/hackathon"
-TRAINING_DIR = f"{MAIN_DIR}/training_dataset"
+TRAINING_DIR = f"{MAIN_DIR}/test_dataset"
 VALIDATION_DIR = f"{MAIN_DIR}/validation_dataset"
 LABEL_FILE_NAME = "true_labels_training.txt"
