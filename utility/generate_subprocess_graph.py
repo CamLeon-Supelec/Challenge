c027dfab9fdba4ab5d7ff24558bdbccbac16b8e4
Benjamin Mouscadet <benjamin.mouscadet@supelec.fr>
2018-11-10 06:38:04 +0100

    FIX : removed fucked up line

--- utility/generate_subprocess_graph.py
+++ utility/generate_subprocess_graph.py
@@ -13,7 +13,6 @@ def generate_subprocess_graph(links):
     """
     G = nx.DiGraph(Type="Subprocesses")
     G.add_edges_from(links)
-    print("Generate subprocesses graph of process " + links[0][1] + " -- Number of links : " + len(links))
     return G
 
 def generate_subprocess_tre(links):
