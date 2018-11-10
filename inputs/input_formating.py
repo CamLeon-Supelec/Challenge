a7101dd00e9b1be193eb54393e372d2ddd0558e2
Japillow <james.barrios@larez.fr>
2018-11-10 08:33:48 +0100

    fixed tree_max_depth

--- inputs/input_formating.py
+++ inputs/input_formating.py
@@ -1,7 +1,6 @@
 import numpy as np
-from statistics import *
+from statistics import median
 import networkx as nx
-
 from bisect import insort
 
 def tree_node_number(tree):
@@ -11,14 +10,15 @@ def tree_node_number(tree):
     """
     if not(nx.is_arborescence(tree)):
         raise ValueError('this should be an arborescence')
-    return nx.number_of_nodes(tree)
+        #erreur
+    return tree.number_of_nodes()
 
 def graph_node_number(graph):
     """
     :param  graph: graph
     :return: the numbre of nodes of the graph
     """
-    return nx.number_of_nodes(graph)
+    return graph.number_of_nodes()
 
 def tree_leaf_number(tree):
     """
@@ -36,21 +36,26 @@ def tree_max_children_number(tree):
     :return: the maximum number of children for a node
     """
     max_number_of_children = 1
-    for node in tree.nodes():
-        number_of_children = nx.degree(tree, node)
+    for node in tree.nodes() :
+        number_of_children = node.degree()
         if number_of_children > max_number_of_children:
             max_number_of_children = number_of_children
     return max_number_of_children
 
-def tree_max_depth(tree, source, depth=1):
+def tree_max_depth(tree):
     """
     :param  tree: tree
     :return: the maximum number of children for a node
     """
-    successors = tree.successors(source)
-    if len(successors) == 0:
-        return depth
-    return np.max([tree_max_depth(tree, succ, depth+1) for succ in successors])
+    max_depth = 1
+    oriented_tree = nx.bfs_tree(tree, source)
+    last_element = [x for x in oriented_tree.nodes()][-1]
+    
+    elt = last_element
+    while elt.in_degree !=0 :
+        elt = tree.predecessors_iter(elt)[0]
+        max_depth+=1
+    return(max_depth)
 
 def graph_structure_stats(graph):
     """
@@ -62,13 +67,13 @@ def graph_structure_stats(graph):
     for list in nx.strongly_connected_components_subgraphs(graph) :
         list_of_length += [len(list)]
 
-    if len(list_of_length) != 0:
+    if len(list_of_length) != 0
         mean = sum(list_of_length)/len(list_of_length)
-        med= median(list_of_length)
-    else:
+        median= median(list_of_length)
+    else 
         mean=0
-        med=0
-    return(len(list_of_length), max(list_of_length), mean, med)
+        median=0
+    return([len(list_of_length),max(list_of_length), mean, median)
 
 
 def generate_api_calls_proximity_matrix(graph):
@@ -98,13 +103,12 @@ def rip_diversity(graph):
     :return: a float between 0 and 1. 1 is high diversity. Lower result means lower RIP diversity
     """
     number_of_api_calls = nx.number_of_nodes(graph)
-    if number_of_api_calls !=0 :
-        set_rips = set()
-        for (rip,api) in graph.nodes :
-            set_rips.add(rip)
-        return len(set_rips)/number_of_api_calls
-    else:
-        return 0
+    if node_number !=0 :
+        set ={}
+        for (rip,api) in graph.nodes : 
+            set.add(rip)
+        return len(set)/number_of_api_calls
+    else return 0
     
 def api_diversity(graph):
     """
@@ -112,13 +116,12 @@ def api_diversity(graph):
     :return: tupple of (float in [0,1] for api diversity, number of different APIs)
     """
     number_of_api_calls = nx.number_of_nodes(graph)
-    if number_of_api_calls !=0 :
-        set_apis = set()
-        for (rip,api) in graph.nodes :
-            set_apis.add(api)
-        return len(set_apis)/number_of_api_calls,len(set_apis)
-    else:
-        return 0
+    if node_number !=0 :
+        set ={}
+        for (rip,api) in graph.nodes : 
+            set.add(api)
+        return len(set)/number_of_api_calls,len(set)
+    else return 0
 
     
 def api_frequency(graph):
@@ -128,13 +131,12 @@ def api_frequency(graph):
     """
     number_of_api = 3561
     number_of_api_calls = nx.number_of_nodes(graph)
+    api_frequency_list = [0. for i in range(number_of_api)]
     if number_of_api_calls != 0 :
-        api_frequency_list = [0. for i in range(number_of_api)]
         for (rip,api) in graph.nodes :
             api = int(api[4:])
             api_frequency_list[api]+=1
         return(np.array(api_frequency_list)/number_of_api_calls)
-    else:
-        return 0
+    else return np.array(api_frequency_list)
 
 
