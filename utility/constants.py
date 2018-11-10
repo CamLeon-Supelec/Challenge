995eca6bbb4fca8b290f7a8aa131d9a2b61605c4
delmassad <matthieu.Delmas@polynom.io>
2018-11-10 03:10:19 +0100

    typo

--- utility/constants.py
+++ utility/constants.py
@@ -2,7 +2,7 @@
 #####LAST MODIFIED BY MATTHIEU#####
 ###################################
 
-MAIN_DIR = f"/home/cloud/hackhaton"
+MAIN_DIR = f"/home/cloud/hackathon"
 TRAINING_DIR = f"{MAIN_DIR}/training_dataset"
 VALIDATION_DIR = f"{MAIN_DIR}/validation_dataset"
 LABEL_FILE_NAME = "true_labels_training.txt"
